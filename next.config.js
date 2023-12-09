055ef8fafe23478c671b4e098b742cbd5d352c4e
Marcin Siwonia <mahinek12@gmail.com>
2023-12-09 20:23:13 +0100

    Add next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = {
+    reactStrictMode: true,
+    experimental: {
+      appDir: true,
+    },
+  };
