0737edc42d07c9c49fb2e5110ee3a56d44a51525
Marcin Siwonia <mahinek12@gmail.com>
2023-12-09 14:18:24 +0100

    Add navbar fixes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+// import Link from 'next/link'; // Usuń tę linijkę, ponieważ nie będziesz używać komponentu Link
 import { MobileSidebar } from './mobile-sidebard';
 import { Button } from './ui/button';
 
@@ -20,24 +20,33 @@ const Navbar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const href = e.currentTarget.getAttribute('href');
+    const targetSection = href ? document.querySelector(href) : null;
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   const textColorClass = navFixed ? "text-anthracit1" : "text-white";
 
   return (
     <div className={`w-full ${navFixed ? "bg-white shadow-md" : "bg-transparent"} fixed top-0 left-0 right-0 z-30 transition-all duration-300`}>
       <nav className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center py-6">
         <span className={`text-xl font-bold ${textColorClass}`}>
-          <Link href="#">Galabau Darius</Link>
+          <a href="#" onClick={handleNavClick}>Galabau Darius</a>
         </span>
         <MobileSidebar />
         <div className="hidden md:flex items-center">
           <span className={`mx-2 text-lg hover:opacity-70 ${textColorClass}`}>
-            <Link href="#gallery">Gallery</Link>
+            <a href="#gallery" onClick={handleNavClick}>Gallery</a>
           </span>
           <span className={`mx-2 text-lg hover:opacity-70 ${textColorClass}`}>
-            <Link href="#konfigurator">Order Form</Link>
+            <a href="#konfigurator" onClick={handleNavClick}>Order Form</a>
           </span>
           <span className={`mx-2 text-lg hover:opacity-70 ${textColorClass}`}>
-            <Link href="#aboutUs">About Us</Link>
+            <a href="#aboutUs" onClick={handleNavClick}>About Us</a>
           </span>
         </div>
       </nav>
