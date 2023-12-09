6e39200b6c3b7c5116aa3b29da2d52462e21ec18
Marcin Siwonia <mahinek12@gmail.com>
2023-12-09 13:20:52 +0100

    Add gallery modal fixes

--- app/@modal/(.)photo/[id]/page.tsx
+++ app/@modal/(.)photo/[id]/page.tsx
@@ -1,48 +1,14 @@
-"use client";
-
-import { useRouter } from 'next/navigation';
-import swagPhotos, { Photo } from '@/lib/photos';
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogClose,
-  DialogFooter
-} from '@/components/ui/dialog';
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
+import Modal from '@/components/modal'
+import swagPhotos, { Photo } from '@/lib/photos'
+import Frame from '@/components/frame'
 
 export default function PhotoModal({ params: { id: photoId } }: { params: { id: string } }) {
-    const router = useRouter();
-
-    const photos = swagPhotos
+  const photos = swagPhotos
   const photo: Photo = photos.find((p) => p.id === photoId)!
 
-
-
-  if (!photo) return null;
-
   return (
-    <Dialog open={true} >
-      <DialogContent>
-        <DialogHeader>
-        </DialogHeader>
-        <Image
-          src={photo.imageSrc}
-          alt={`Gallery image ${photo.id}`}
-          width={800} // Zdefiniuj odpowiednią szerokość
-          height={600} // Zdefiniuj odpowiednią wysokość
-        />
-                <DialogFooter className="sm:justify-start">
-          <DialogClose asChild>
-            <Button  onClick={() => router.push('/', { scroll: false })}  variant="secondary">
-              Close
-            </Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
-};
-
+    <Modal>
+      <Frame photo={photo} />
+    </Modal>
+  )
+}
--- components/modal.tsx
+++ components/modal.tsx
@@ -1,60 +1,36 @@
-"use client";
-import { useState, useCallback, useRef, useEffect, MouseEvent, ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
+'use client'
+import { useCallback, useRef, useEffect, MouseEventHandler } from 'react'
+import { useRouter } from 'next/navigation'
 
-interface ModalProps {
-  children: ReactNode;
-}
-
-export default function Modal({ children }: ModalProps) {
-  const overlay = useRef<HTMLDivElement>(null);
-  const wrapper = useRef<HTMLDivElement>(null);
-  const router = useRouter();
-  const [scrollPosition, setScrollPosition] = useState(0);
+export default function Modal({ children }: { children: React.ReactNode }) {
+  const overlay = useRef(null)
+  const wrapper = useRef(null)
+  const router = useRouter()
 
   const onDismiss = useCallback(() => {
-    closeModal();
-    router.back();
-  }, [router]);
+    router.back()
+  }, [router])
 
-  const onClick = useCallback(
-    (e: MouseEvent) => {
+  const onClick: MouseEventHandler = useCallback(
+    (e) => {
       if (e.target === overlay.current || e.target === wrapper.current) {
-        onDismiss();
+        if (onDismiss) onDismiss()
       }
     },
     [onDismiss, overlay, wrapper]
-  );
+  )
 
   const onKeyDown = useCallback(
-    (e: KeyboardEvent) => { // Użyj globalnego KeyboardEvent
-      if (e.key === 'Escape') {
-        onDismiss();
-      }
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') onDismiss()
     },
     [onDismiss]
-  );
+  )
 
   useEffect(() => {
-    // Zapisz pozycję i zablokuj przewijanie przy montowaniu komponentu
-    setScrollPosition(window.pageYOffset);
-    document.body.style.overflow = 'hidden';
-
-    document.addEventListener('keydown', onKeyDown as EventListener); // Rzutowanie na EventListener
-    return () => {
-      document.removeEventListener('keydown', onKeyDown as EventListener); // Rzutowanie na EventListener
-
-      // Przywróć przewijanie przy demontowaniu komponentu
-      document.body.style.overflow = 'unset';
-      window.scrollTo(0, scrollPosition);
-    };
-  }, [onKeyDown, scrollPosition]);
-
-  const closeModal = () => {
-    // Funkcja do przywracania przewijania, ale nie jest wywoływana bezpośrednio w useEffect
-    document.body.style.overflow = 'unset';
-    window.scrollTo(0, scrollPosition);
-  };
+    document.addEventListener('keydown', onKeyDown)
+    return () => document.removeEventListener('keydown', onKeyDown)
+  }, [onKeyDown])
 
   return (
     <div
@@ -69,5 +45,5 @@ export default function Modal({ children }: ModalProps) {
         {children}
       </div>
     </div>
-  );
+  )
 }
