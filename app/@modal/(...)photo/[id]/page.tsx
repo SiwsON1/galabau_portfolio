4124037414dca8e0511a94659b35df2c2345bac5
Marcin Siwonia <mahinek12@gmail.com>
2023-12-09 20:28:31 +0100

    Add another intercepting fix

--- app/@modal/(...)photo/[id]/page.tsx
+++ app/@modal/(...)photo/[id]/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import swagPhotos, { Photo } from '../../../lib/photos'
-import Frame from '../../../components/frame'
-import Modal from '../../../components/modal'
+import swagPhotos, { Photo } from '../../../../lib/photos'
+import Frame from '../../../../components/frame'
+import Modal from '../../../../components/modal'
 
 export default function PhotoModal({ params: { id: photoId } }: { params: { id: string } }) {
   const photos = swagPhotos
