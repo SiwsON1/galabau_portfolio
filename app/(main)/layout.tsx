e8dab6c08a75af986073ae48ac91f63502df387b
Marcin Siwonia <mahinek12@gmail.com>
2023-12-29 16:01:51 +0100

    Add layout fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,30 +3,25 @@ import React from 'react';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-
-
-interface RootLayoutProps {
-  children: React.ReactNode;
-  modal?: React.ReactNode; // Dodanie propa modal jako opcjonalnego
-}
-
 export default function RootLayout({
   children,
   modal
-}: RootLayoutProps) {
+}: {
+  children: React.ReactNode;
+  modal?: React.ReactNode;
+}) {
   return (
-        <div className="h-full ">
-          <div className="fixed  mx-auto z-50">
-            <Navbar />
-          </div>
-          <main className="flex justify-center overflow-hidden ">
-            <div className="max-w-[1440px] bg-white w-full">
-              {children}
-              {modal && modal}
-            </div>
-          </main>
-          <Footer />
+    <div className="h-full">
+      <div className="fixed mx-auto z-50">
+        <Navbar />
+      </div>
+      <main className="flex justify-center overflow-hidden">
+        <div className="max-w-[1440px] bg-white w-full">
+          {children}
+          {modal && modal}
         </div>
-
+      </main>
+      <Footer />
+    </div>
   );
 }
