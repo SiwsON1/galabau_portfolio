1896f7834350a48926b712f50ee832e96d7ed2f9
Marcin Siwonia <mahinek12@gmail.com>
2024-01-06 19:04:52 +0100

    Finally removed layout issue

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-export default function RootLayout({
+export default function MainLayout({
   children,
   modal,
 }: {
