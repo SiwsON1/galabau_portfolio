3ff86d5a3d7e08d1baed7b23caffc2c72dc50479
Marcin Siwonia <mahinek12@gmail.com>
2024-01-06 19:06:57 +0100

    Add modal fixes

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ export default function MainLayout({
   modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
+  modal?: React.ReactNode;
 }) {
   return (
     <div className="h-full">
